51862953a1546195b629c2fe3d78b353d972d29b
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-04-10 23:30:26 +0900

    test

--- src/main/java/com/board/BoardApplication.java
+++ src/main/java/com/board/BoardApplication.java
@@ -2,11 +2,18 @@ package com.board;
 
 import com.board.dbConfig.DBConnentConfig;
 import com.board.dbConfig.MariaDBConnectConfig;
+import com.board.domain.PostInfo;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class BoardApplication {
     public static void main(String[] args) {
@@ -14,20 +21,33 @@ public class BoardApplication {
         PreparedStatement pstmt = null;
         ResultSet rs = null;
 
+        DBConnentConfig dbConfig = new MariaDBConnectConfig();
+        conn = dbConfig.connectDB();
 
-        try{
-            DBConnentConfig dbConfig = new MariaDBConnectConfig();
-            conn = dbConfig.connectDB();
-
-            String insertSQL = "insert into category_info (category_name) values('hello world')";
-
-            pstmt = conn.prepareStatement(insertSQL);
-
-            rs = pstmt.executeQuery();
-
-        }catch (SQLException e){
-            e.printStackTrace();
-        }finally {
+        String sql = "select post_id, category_id, title, post_content, writer, write_dt, update_dt from post_info";
+		List<PostInfo> postList = new LinkedList<PostInfo>();
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				PostInfo post = new PostInfo();
+				post.setPostId(rs.getInt(1));
+				post.setCategoryId(rs.getInt(2));
+				post.setTitle(rs.getString(3));
+				post.setPostContent(rs.getString(4));
+				post.setWriter(rs.getString(5));
+				Date writeDt = rs.getDate(6);
+				post.setWriteDt(new Timestamp(writeDt.getTime()).toLocalDateTime());
+				Date updateDt = rs.getDate(7);
+				post.setUpdateDt(new Timestamp(updateDt.getTime()).toLocalDateTime());
+				
+				postList.add(post);
+			}
+			System.out.println(postList.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
             try{
                 if(rs != null)rs.close();
                 if(pstmt != null)pstmt.close();
