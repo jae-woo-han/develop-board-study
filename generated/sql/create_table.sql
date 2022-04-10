f923d2ef80454fbf2aedf10d0327442f4197f0f1
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-04-10 23:15:32 +0900

    Update create_table.sql
    dbeaver에서 DLL 가져옴

--- generated/sql/create_table.sql
+++ generated/sql/create_table.sql
@@ -1,35 +1,39 @@
-CREATE TABLE post_info(
-	post_id INT NOT NULL PRIMARY KEY,
-	category_id INT NOT NULL,
-	title VARCHAR(100) NOT NULL,
-	post_content VARCHAR(2000) NOT NULL,
-	writer VARCHAR(5) NOT NULL,
-	password VARCHAR(16) NOT NULL,
-	write_dt TIMESTAMP NOT NULL DEFAULT NOW(),
-	update_dt TIMESTAMP NOT NULL DEFAULT NOW(),
-	view_count INT NOT NULL
+CREATE TABLE `post_info` (
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `post_content` varchar(2000) NOT NULL,
+  `writer` varchar(5) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `write_dt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_dt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `view_count` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`post_id`)
 );
 	
-CREATE TABLE category_info(
-	 category_id INT NOT NULL PRIMARY KEY,
-	 category_name VARCHAR(20) NOT NULL,
-	 create_dt TIMESTAMP NOT NULL DEFAULT NOW()
+CREATE TABLE `category_info` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(20) NOT NULL,
+  `create_dt` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`category_id`)
 );
 	 
 	 
-CREATE TABLE comment_info(
-	comment_id INT NOT NULL PRIMARY KEY,
-	post_id INT NOT NULL,
-	comment_content VARCHAR(1000) NOT NULL,
-	write_dt TIMESTAMP NOT NULL DEFAULT NOW()
+CREATE TABLE `comment_info` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) NOT NULL,
+  `comment_content` varchar(1000) NOT NULL,
+  `write_dt` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`comment_id`)
 );
 
-CREATE TABLE file_info(
-	file_seq INT NOT NULL PRIMARY KEY,
-	post_id INT NOT NULL,
-	file_name VARCHAR(100) NOT NULL,
-	file_type VARCHAR(10) NOT NULL,
-	encode_file_name VARCHAR(300) NULL,
-	directory_path VARCHAR(100) NOT NULL,
-	save_dt TIMESTAMP NOT NULL DEFAULT NOW()
+CREATE TABLE `file_info` (
+  `file_seq` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) NOT NULL,
+  `file_name` varchar(100) NOT NULL,
+  `file_type` varchar(10) NOT NULL,
+  `encode_file_name` varchar(300) DEFAULT NULL,
+  `directory_path` varchar(100) NOT NULL,
+  `save_dt` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`file_seq`)
 );
